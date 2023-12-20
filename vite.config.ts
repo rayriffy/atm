c300f7c2bcea67d4423b8f9c39ccef4367a68b1c
Phumrapee Limpianchop <git@rayriffy.com>
2023-12-21 07:08:35 +0900

    fix: base

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.CI === 'true' ? '' : undefined,
+  base: process.env.CI === 'true' ? '/atm' : undefined,
   plugins: [react(), generouted(), tsconfigPaths()],
 })
